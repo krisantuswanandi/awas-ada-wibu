ad6aa1b7330262b9f58a29ad888db8b46f6d193c
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2025-07-01 21:02:32 +0700

    revamp login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,6 +2,10 @@
 const config = useRuntimeConfig();
 const auth = useAuth();
 
+definePageMeta({
+  layout: false,
+});
+
 const anilistClientId = config.public.anilistClientId;
 const anilistAuthUrl = `https://anilist.co/api/v2/oauth/authorize?client_id=${anilistClientId}&response_type=token`;
 
@@ -13,7 +17,26 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <a :href="anilistAuthUrl">Login</a>
-  </div>
+  <NuxtLayout name="blank">
+    <div class="flex flex-col items-center justify-center p-4 min-h-dvh">
+      <UButton
+        :to="anilistAuthUrl"
+        external
+        class="bg-slate-700 hover:bg-slate-800 dark:bg-slate-100 dark:hover:bg-slate-200 gap-0 p-1 group"
+      >
+        <div
+          class="h-6 w-6 p-0.5 bg-slate-700 group-hover:bg-slate-800 transition-colors rounded"
+        >
+          <NuxtImg
+            src="https://anilist.co/img/icons/icon.svg"
+            alt="AniList Logo"
+            class="h-full w-full object-contain"
+            loading="lazy"
+          />
+        </div>
+        <div class="px-2">Login with AniList</div>
+      </UButton>
+      <AppFooter class="mt-10" />
+    </div>
+  </NuxtLayout>
 </template>
